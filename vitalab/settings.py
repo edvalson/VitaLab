8edfc953cf8abdd7c72e5d375c3e997887d06538
Edvalson Ribeiro <edvalsonjr@gmail.com>
2023-12-05 04:04:01 -0300

    correções

--- vitalab/settings.py
+++ vitalab/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,13 +86,6 @@ DATABASES = {
     }
 }
 
-"""
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
-}
-
-"""
 
 """
 DATABASES = {
