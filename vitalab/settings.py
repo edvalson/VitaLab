16a3f83d3ae60ab64526052b9e418b53530d35f6
Edvalson Ribeiro <edvalsonjr@gmail.com>
2023-12-05 03:00:26 -0300

    Correções

--- vitalab/settings.py
+++ vitalab/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'vitalab.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -88,7 +89,15 @@ DATABASES = {
 """
 
 DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+}
+
+"""
+
+"""
+DATABASES = {
     'default': {
+       
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "vitalab",
         "USER": "postgres",
@@ -97,6 +106,8 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+
+"""
 
 
 
